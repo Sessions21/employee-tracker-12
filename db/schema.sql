dab770fe7d485d101b23c5d2f95b899de474c0f2
Scott Nichols <s21nichols@hotmail.com>
2022-01-26 01:13:49 -0700

    corrected some schema constraints

--- db/schema.sql
+++ db/schema.sql
@@ -8,11 +8,10 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-  id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL UNSIGNED NOT NULL,
-  department_id INTEGER UNSIGNED NOT NULL,
-  INDEX dep_ind (department_id),
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
@@ -20,10 +19,8 @@ CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER UNSIGNED NOT NULL,
-  INDEX role_ind (role_id),
+  role_id INTEGER,
   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-  manager_id INTEGER UNSIGNED,
-  INDEX man_ind (manager_id),
+  manager_id INTEGER,
   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
